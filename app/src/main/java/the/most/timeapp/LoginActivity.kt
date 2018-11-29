7167b110a0faacac08ec93685354b285b81e8dce
noSerious217 <gidrolisk314@yandex.ru>
2018-11-29 22:16:56 +0400

    1

---
+++ app/src/main/java/the/most/timeapp/LoginActivity.kt
@@ -0,0 +1,12 @@
+package the.most.timeapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
