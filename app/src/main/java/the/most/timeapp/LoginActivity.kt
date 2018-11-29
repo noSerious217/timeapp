292dcbf8acef5e07af5067901a43ac8966c8b5f0
LuisLustig <aksi-nait@yandex.com>
2018-11-30 00:32:42 +0400

    Тупая, не проиндекировала все файлы лол

--- app/src/main/java/the/most/timeapp/LoginActivity.kt
+++ app/src/main/java/the/most/timeapp/LoginActivity.kt
@@ -1,12 +1,20 @@
 package the.most.timeapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+
+    fun onLoginClick(view: View)
+    {
+        val mainIntent = Intent(this, MainActivity::class.java)
+        startActivity(mainIntent)
     }
 }
--- app/src/main/java/the/most/timeapp/MainActivity.kt
+++ app/src/main/java/the/most/timeapp/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            Snackbar.make(view, getString(R.string.testStr), Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
     }
