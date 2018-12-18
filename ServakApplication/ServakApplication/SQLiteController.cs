2e98272bda49df9dda82b23ee95ab10a7c2721e4
noSerious217 <gidrolisk314@yandex.ru>
2018-12-18 21:55:14 +0400

    Работа с бд

---
+++ ServakApplication/ServakApplication/SQLiteController.cs
@@ -0,0 +1,79 @@
+﻿using ServakApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServakApplication
+{
+    public static class SQLiteController
+    {
+        private static String dbFileName = "themostdb.sqlite";
+        private static SQLiteConnection m_dbConn;
+        private static SQLiteCommand m_sqlCmd;
+
+        public static void init()
+        {
+            m_dbConn = new SQLiteConnection();
+            m_sqlCmd = new SQLiteCommand();
+            if (!File.Exists(dbFileName))
+                SQLiteConnection.CreateFile(dbFileName);
+            try
+            {
+                m_dbConn = new SQLiteConnection("Data Source=" + dbFileName + ";Version=3;");
+                m_dbConn.Open();
+                m_sqlCmd.Connection = m_dbConn;
+
+                m_sqlCmd.CommandText = "create table if not exists timeevent(id integer primary key autoincrement, begin text, end text, eventname text, colot text)";
+                m_sqlCmd.ExecuteNonQuery();
+            } 
+            catch (Exception e)
+            {
+                //TODO
+            }
+        }
+
+        public static void insert(TimeEventSpan span)
+        {
+            try
+            {
+                m_sqlCmd.CommandText = "insert into timeevent(begin, end, eventname, color) values ('"+DateTimeToString(span.Begin)+"','"+DateTimeToString(span.End)+"','"+span.EventName+"','"+span.Color+"')";
+                m_sqlCmd.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                //TODO
+            }
+        }
+
+        public static string DateTimeToString(DateTime time)
+        {
+            return time.Hour.ToString() + ':' + time.Minute.ToString();
+        }
+
+        public static DateTime StringToDateTime(string time)
+        {
+            string[] timearr = time.Split(':');
+            int hour = int.Parse(timearr[0]);
+            int minute = int.Parse(timearr[1]);
+            DateTime current = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, hour, minute, DateTime.Now.Second);
+            return current;
+        }
+
+        public static List<TimeEventSpan> select()
+        {
+            DataTable dTable = new DataTable();
+            SQLiteDataAdapter adapter = new SQLiteDataAdapter("select * from timeevent",m_dbConn);
+            adapter.Fill(dTable);
+            List<TimeEventSpan> list = new List<TimeEventSpan>();
+            for (int i=0;i<dTable.Rows.Count;i++)
+            {
+                list.Add(new TimeEventSpan() { Id = (int)dTable.Rows[i].ItemArray[0], Begin = StringToDateTime((string)dTable.Rows[i].ItemArray[1]), End = StringToDateTime((string)dTable.Rows[i].ItemArray[2]), EventName=(string)dTable.Rows[i].ItemArray[3],Color=(string)dTable.Rows[i].ItemArray[4]});
+            }
+            return list;
+        }
+    }
+}
--- ServakApplication/ServakApplication/Startup.cs
+++ ServakApplication/ServakApplication/Startup.cs
@@ -1,24 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.SQLite;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace ServakApplication
 {
     public class Startup
     {
-        private string dbFileName = "themostdb.sqlite";
-        private SQLiteConnection m_dbConn;
-        private SQLiteCommand m_sqlCmd;
-
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -41,25 +29,6 @@ namespace ServakApplication
             }
 
             app.UseMvc();
-
-            m_dbConn = new SQLiteConnection();
-            m_sqlCmd = new SQLiteCommand();
-            if (!File.Exists(dbFileName))
-                SQLiteConnection.CreateFile(dbFileName);
-            try
-            {
-                m_dbConn = new SQLiteConnection("Dara Source=" + dbFileName + ";Version=3;");
-                m_dbConn.Open();
-                m_sqlCmd.Connection = m_dbConn;
-
-                m_sqlCmd.CommandText = "create table if not exists user (id integer primary key autoincrement, name text)";
-                m_sqlCmd.ExecuteNonQuery();
-
-            }
-            catch (SQLiteException ex)
-            {
-                //TODO
-            }
         }
     }
 }
